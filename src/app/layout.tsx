36835fd7f98d697fe5991e4035b7bdc09b88c122
Zhijiang Li <zl256@cornell.edu>
2024-04-16 15:07:42 -0500

    fix hydration error, add div padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,49 +13,47 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} mx-auto max-w-5xl px-6 sm:px-8 lg:px-12`}
-      >
-        <RestaurantTitle name="Vietnamese Delight" />
-        <main className="text-center mb-12">
-          <div className="flex justify-evenly items-center mb-4">
-            <h2 className="text-xl font-bold mb-4 px-2">
-              <Link href="/about" className="hover:underline">
-                About
-              </Link>
-            </h2>
-            <h2 className="text-xl font-bold mb-4 px-2">
-              <Link href="/" className="hover:underline">
-                Menu
-              </Link>
-            </h2>
-            <h2 className="text-xl font-bold mb-4 px-2">
-              <Link
-                target="_blank"
-                href="https://www.yelp.com/biz/vietnamese-delight-los-angeles-2"
-                rel="noopener noreferrer"
-                className="hover:underline"
-              >
-                Yelp
-              </Link>
-            </h2>
-            <h2 className="text-xl font-bold mb-4 px-2">
-              <Link
-                target="_blank"
-                href="https://www.ubereats.com/store/vietnamese-delight-358-w-38th-st/5qOZv-i2RL6G2blPIkGCAw?diningMode=DELIVERY"
-                rel="noopener noreferrer"
-                className="hover:underline"
-              >
-                Order
-              </Link>
-            </h2>
-          </div>
-          {children}
-        </main>
-        <SpeedInsights />
-        <Analytics />
-      </body>
-    </html>
+    <div
+      className={`${inter.className} mx-auto max-w-5xl px-6 sm:px-8 lg:px-12`}
+    >
+      <RestaurantTitle name="Vietnamese Delight" />
+      <main className="text-center mb-12">
+        <div className="flex justify-evenly items-center mb-4 md:px-8 lg:px-16">
+          <h2 className="text-xl font-bold mb-4 px-2">
+            <Link href="/about" className="hover:underline">
+              About
+            </Link>
+          </h2>
+          <h2 className="text-xl font-bold mb-4 px-2">
+            <Link href="/" className="hover:underline">
+              Menu
+            </Link>
+          </h2>
+          <h2 className="text-xl font-bold mb-4 px-2">
+            <Link
+              target="_blank"
+              href="https://www.yelp.com/biz/vietnamese-delight-los-angeles-2"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Yelp
+            </Link>
+          </h2>
+          <h2 className="text-xl font-bold mb-4 px-2">
+            <Link
+              target="_blank"
+              href="https://www.ubereats.com/store/vietnamese-delight-358-w-38th-st/5qOZv-i2RL6G2blPIkGCAw?diningMode=DELIVERY"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Order
+            </Link>
+          </h2>
+        </div>
+        {children}
+      </main>
+      <SpeedInsights />
+      <Analytics />
+    </div>
   );
 }
